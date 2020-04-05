c6d175ff2bd5fd022c552af894207a93ada08502
Ayoola David <ayooladavid@gmail.com>
2020-04-05 23:30:00 +0100

    Dev

---
+++ J-DOM-TEST/nodes/script.js
@@ -0,0 +1,9 @@
+let button = document.getElementById('changeBackground');
+
+button.addEventListener('click', () => {
+	document.body.style.backgroundColor = 'fuchsia'
+})
+
+document.addEventListener('dbClick', () => {
+	document.body.style.backgroundColor = 'white'
+})
