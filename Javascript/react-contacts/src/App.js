ff48d002ca2962a115eda47ad3fd7dcd417bc87c
Ayoola David <ayooladavid@gmail.com>
2020-04-19 23:15:03 +0100

    add changes

--- Javascript/react-contacts/src/App.js
+++ Javascript/react-contacts/src/App.js
@@ -1,42 +1,49 @@
-import React, {Component} from 'react';
-import ListContacts from './ListContact';
-
+import React, {Component} from 'react'
+import * as ContactsAPI from './utils/ContactsAPI'
+import { Route } from 'react-router-dom'
+import ListContacts from './ListContact'
+import CreateContact from './CreateContact'
 
 
 class App extends Component {
   state = { 
-    contacts : [
-      {
-        "id": "ryan",
-        "name": "Dan Florence",
-        "email": "ryan@reacttraining.com",
-        "avatarURL": "http://localhost:5001/ryan.jpg"
-      },
-      {
-        "id": "michael",
-        "name": "Michael Jackson",
-        "email": "michael@reacttraining.com",
-        "avatarURL": "http://localhost:5001/michael.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "email": "tyler@reacttraining.com",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-    ]
+
+    screen : '',
+
+    contacts : []
+  }
+
+  componentDidMount(){
+    ContactsAPI.getAll().then((contacts) => {
+      this.setState({ contacts })
+    })
   }
 
   deleteContact = (contact) => {
     this.setState(state => ({
       contacts: state.contacts.filter((person) => person.id !== contact.id )
       }))
+
+    ContactsAPI.remove(contact)
+  }
+
+
+  changePage = () => {
+    this.setState({currentPage: "home"})
   }
 
   render(){
     return (
-      <div>
-      <ListContacts contacts={this.state.contacts} deleteContact={this.deleteContact}/>
+      <div className="app">
+        <Route exact path="/" render={() => (
+          <ListContacts
+            contacts={this.state.contacts}
+            deleteContact={this.deleteContact}
+            onNavigate={() => {
+              this.setState({screen: 'create'})
+            }}/>
+          )} />
+        <Route exact path="/create" render={() => (<CreateContact />)}/>
       </div>
 
     )
