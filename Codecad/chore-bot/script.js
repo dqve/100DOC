4f8c3cad1017910d27304e060c691cd54fb0326a
Ayoola David <ayooladavid@gmail.com>
2020-05-24 02:21:59 +0100

    Chore bot update

--- Codecad/chore-bot/script.js
+++ Codecad/chore-bot/script.js
@@ -10,73 +10,85 @@ const spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chor
 
 const startButton = document.getElementById('start');
 
-const randomChoreDoorGenerator = ()=>{
+const randomChoreDoorGenerator = () => {
     const numClosedDoors = 3;
     const choreDoor = Math.floor(Math.random() * numClosedDoors);
 
-    if(choreDoor === 0) {
-    openDoorA = botDoorPath;
-    openDoorB = beachDoorPath;
-    openDoorC = spaceDoorPath;
-  } else if (choreDoor === 1) {
-    openDoorB = botDoorPath;
-    openDoorC = beachDoorPath;
-    openDoorA = spaceDoorPath;
-  } else if (choreDoor === 2) {
-    openDoorC = botDoorPath;
-    openDoorA = beachDoorPath;
-    openDoorB = spaceDoorPath;
-  } 
-  console.log(choreDoor)
-	const imageList = [openDoorA,openDoorB,openDoorC]
-
-	for (let i = 0; i < doorImages.length; i++){
-	  let anImage = doorImages[i]
-	  let anImagePic = imageList[i]
-	  anImage.onclick = () => {
-	    if (currentlyPlaying && !isClicked(anImage)){
-          anImage.src = imageList[i]
-          playDoor(doorImages[i])
-        }}
-    startButton.onclick = () => {
-      startRound()
+    if (choreDoor === 0) {
+        openDoorA = botDoorPath;
+        openDoorB = beachDoorPath;
+        openDoorC = spaceDoorPath;
+    } else if (choreDoor === 1) {
+        openDoorB = botDoorPath;
+        openDoorC = beachDoorPath;
+        openDoorA = spaceDoorPath;
+    } else if (choreDoor === 2) {
+        openDoorC = botDoorPath;
+        openDoorA = beachDoorPath;
+        openDoorB = spaceDoorPath;
     }
-  }
+    console.log(choreDoor)
+    const imageList = [openDoorA, openDoorB, openDoorC]
+
+    for (let i = 0; i < doorImages.length; i++) {
+        let anImage = doorImages[i]
+        let anImagePic = imageList[i]
+        anImage.onclick = () => {
+            if (currentlyPlaying && !isClicked(anImage)) {
+                anImage.src = imageList[i]
+                playDoor(doorImages[i])
+            }
+        }
+        startButton.onclick = () => {
+            startRound()
+        }
+
+    }
+}
+
+
+const startRound = () => {
+    numClosedDoors = doorImages.length;
+    startButton.innerHTML = 'Good luck!'
+    currentlyPlaying = true
+    doorImages.forEach( animage => animage.src = closedDoorPath)
+    randomChoreDoorGenerator()
+
 }
 
 const gameOver = (status) => {
-      if(status === 'win') {
+    if (status == 'win') {
         startButton.innerHTML = 'You win! Play again?';
-      }else{
+    } else {
         startButton.innerHTML = 'Game over! Play again?';
-      }
-      currentlyPlaying = false
     }
+    currentlyPlaying = false
+}
 
 const isBot = (door) => {
-  if(door.src == botDoorPath){
-    return true
-  }else{
-    return false
-  }
+    if (door.src == botDoorPath) {
+        return true
+    } else {
+        return false
+    }
 }
 
 const isClicked = (door) => {
-  closedDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg"
-  if(door.src == closedDoorPath){
-    return false
-  }else{
-    return true
-  }
+    closedDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg"
+    if (door.src == closedDoorPath) {
+        return false
+    } else {
+        return true
+    }
 }
 
 const playDoor = (door) => {
-  numClosedDoors--
-  if (numClosedDoors === 0){
-    gameOver('win')
-  }else if (isBot(door)){
-    gameOver()
-  }
+    numClosedDoors--
+    if (numClosedDoors == 0) {
+        gameOver('win')
+    } else if (isBot(door)) {
+        gameOver()
+    }
 
 }
 
